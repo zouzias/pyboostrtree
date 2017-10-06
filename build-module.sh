b9c4400558061a123d100fb4cccf2c190ad1486f
Anastasios Zouzias <zouzias@gmail.com>
2017-10-06 21:03:04 +0200

    move to dir boostrtrees

--- build-module.sh
+++ build-module.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+
 python setup.py build_ext --inplace
 
--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 
-import sys, os
+import sys, os, re, io
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
 import numpy
+
+__version__ = re.search(
+    r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]',  # It excludes inline comment too
+    io.open('boostrtrees/__init__.py', encoding='utf_8').read()
+    ).group(1)
+
 
 compile_args = ['-g', '-std=c++11', '-stdlib=libc++']
 
@@ -17,9 +23,16 @@ if 'BOOST_ROOT' not in os.environ:
 
 print('BOOST_ROOT = {}'.format(os.environ['BOOST_ROOT']))
 
-setup(cmdclass = {'build_ext': build_ext},
+setup(
+    author="Anastasios Zouzias",
+    version=__version__,
+    url="https://github.com/zouzias/pyboostrtree.git",
+    description="Python Wrapper of Boost Geometry Rtree",
+    license="Apache 2.0",
+    packages = ['boostrtrees'],
+    cmdclass = {'build_ext': build_ext},
                 ext_modules = [Extension("boostrtrees",
-                sources=["boostrtrees.pyx", "RTreePoint2D.cpp"],
+                sources=["boostrtrees/boostrtrees.pyx", "boostrtrees/RTreePoint2D.cpp"],
                 language="c++",
                 extra_compile_args=compile_args,
                 include_dirs=[numpy.get_include(), os.environ['BOOST_ROOT'] + '/include'])],
