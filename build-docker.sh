d36d8f0627b3fa754a0dc22e817a2171a4ee3c7b
Anastasios Zouzias <zouzias@users.noreply.github.com>
2017-11-08 07:34:29 +0100

    build-docker: add comment

--- build-docker.sh
+++ build-docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Build python package inside an Ubuntu docker container
+
 docker build docker-python/ -t boostrtrees
 docker run -t -v `pwd`:/opt/python-package/ boostrtrees make clean
 docker run -t -v `pwd`:/opt/python-package/ boostrtrees python3 setup.py sdist bdist_wheel
