2f5687657d21a6d5b656ede5a483dad0530e59b9
Anastasios Zouzias <zouzias@gmail.com>
2017-11-08 20:45:17 +0100

    use_manylinux1: build on i686/x86_64

--- build-manylinux1.sh
+++ build-manylinux1.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 make clean
-docker build docker-manylinux1/ -t manylinux1
-docker run --rm -v `pwd`:/io manylinux1 /io/travis/build-wheels.sh
+docker build docker-manylinux1_x86_64/ -t manylinux1_x86_64
+docker run --rm -v `pwd`:/io manylinux1_x86_64 /io/travis/build-wheels.sh
+
+export PRE_CMD=linux32
+docker build docker-manylinux1_i686/ -t manylinux1_i686
+docker run --rm -v `pwd`:/io manylinux1_i686 /io/travis/build-wheels.sh
