7ff31335e0abfcbf8bc87caff0deec3944be5e9c
Anastasios Zouzias <zouzias@gmail.com>
2018-02-18 19:44:36 +0100

    scripts: minor fix

--- build-and-benchmark.sh
+++ build-and-benchmark.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-# Builds and test the python package within  a virtualenv
+# Build and bechmark the python package within a virtualenv
 
 # Package version
 VERSION=`cat version.py | awk '{print $3}' | sed "s/'//g"`
 
-rm -rf sdist/
+rm -rf env/
+rm -rf dist/
 
 make clean
 pip install cython disttools
 python setup.py sdist
 
-virtualenv sdist
-source sdist/bin/activate
-sdist/bin/pip install numpy cython pandas
-sdist/bin/pip install  --upgrade dist/boostrtrees-${VERSION}.tar.gz
+virtualenv env/
+source env/bin/activate
+env/bin/pip install numpy cython pandas
+env/bin/pip install  --upgrade dist/boostrtrees-${VERSION}.tar.gz
 cd benchmarks/ && python benchmark.py
